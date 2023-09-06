3cca53541b437a43caeb823abf814e38e93f81a2
Dustin Freeman <dustin.freeman@gmail.com>
2023-09-06 14:54:41 -0400

    support for multiple videos

--- Assets/Scripts/LoadVideo.cs
+++ Assets/Scripts/LoadVideo.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Video;
 
 public class LoadVideo : MonoBehaviour
 {
     [SerializeField]
-    string videoName;
+    protected string videoName;
+    [SerializeField]
+    protected int videoWidth = 512;
+    [SerializeField]
+    protected int videoHeight = 512;
 
     void Start()
     {
         var videoPlayer = GetComponent<VideoPlayer>();
+        var meshRenderer = GetComponent<MeshRenderer>();
+
+        var renderTexture = new RenderTexture(videoWidth, videoHeight, 32);
+        videoPlayer.targetTexture = renderTexture;
+
+        //copying the material here, since if you want to play multiple videos, you don't want them all writing to the same material.
+        meshRenderer.material = new Material(meshRenderer.material);
+        meshRenderer.material.mainTexture = renderTexture;
+
         videoPlayer.url = Application.streamingAssetsPath + "/" + videoName;
     }
 }
